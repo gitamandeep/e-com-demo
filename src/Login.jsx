7b8f06ce85f04c5c068cce4e985adfcdf46f5f0d
amandeep7421101 <amandeep@electrowaveselectronics.com>
2024-02-14 20:23:55 +0530

    change place holder from email to userName

--- src/Login.jsx
+++ src/Login.jsx
@@ -37,10 +37,10 @@ function Login() {
   return (
     <Form className='main' onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
+        <Form.Label>User Name</Form.Label>
         <Form.Control
           type="text"
-          placeholder="Enter email"
+          placeholder="Enter Username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
